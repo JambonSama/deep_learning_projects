4565a1b5e975a4964209d91440bb0042710ac8c9
Nicolas <nicolas.furrer@epfl.ch>
2020-05-21 00:34:33 +0200

    Added : separation of the code refactoring

---
+++ Proj2/layer.py
@@ -0,0 +1,172 @@
+import numpy as np
+import torch
+
+# Class definition
+class Linear:
+    """
+    Linear class : create a linear layer of the neural network
+    Use random distribution with mean = 0 variance = 1 to initialize weight,
+    bias initialize with 0 
+    Parameters :
+        nb_input -- number of neuron in input
+        nb_output -- number of neuron in output
+    """
+    def __init__(self, nb_input, nb_output):
+        self.layer_type = "Linear"
+        self.variance = 1
+        self.nb_input = nb_input
+        self.nb_output = nb_output
+        self.weight = np.sqrt(self.variance)*torch.randn(nb_input, nb_output)
+        self.bias = torch.zeros(nb_output)
+
+    def activation(self,x):
+        """
+        Linear activation function output = input
+        Parameters : 
+            x -- input value
+        Retrun : 
+            out -- result of activation for x
+        """
+        out = x
+        return out
+
+    def d_activation(self, x):
+        """
+        Derivate of linear activation function 
+        Parameters : 
+            x -- value to compute the dervate activation
+        Retrun : 
+            1
+        """
+        return 1
+
+    def forward_pass(self,x):
+        """
+        Compute the forward pass, activation(weight * input + bias)
+        Parameters : 
+            x -- input value
+        Retrun : 
+            out -- result of activation(weight * x + bias)
+        """
+        self.input_matrix = x
+        self.z = torch.matmul(x, self.weight)+self.bias
+        out = self.activation(self.z)
+        self.output_matrix = out
+        return out
+    
+    def backward_pass(self, dl_dy):
+        """
+        Compute the backward pass, compute derivative of loss, accumulate de derivative of 
+        the loss in function of the weight and the bias
+        Parameters : 
+            dl_dy -- derivative of the loss in function of the output
+        Retrun : 
+            dl_dx -- derivative of the loss in function of the input
+        """
+        dl_dz = self.d_activation(self.z)
+        dl_dx = torch.matmul(dl_dz*dl_dy,self.weight.T)
+        dl_dw = torch.matmul(self.input_matrix.view(-1,1), (dl_dz*dl_dy).view(1,-1))
+        dl_db = dl_dz*dl_dy
+        # Gradian accumulation
+        self.acc_weight = self.acc_weight + dl_dw
+        self.acc_bias = self.acc_bias + dl_db
+        return dl_dx
+    
+    def update_parameters(self, learning_rate, batch_size):
+        """
+        Update the weight and the bias of the layer in function of the learning rate, batch size
+        and the accumulated weight and bias
+        Parameters : 
+            learning_rate -- learning rate of the neural network
+            batch_size -- size of the batch where the gradian was accumulate
+        """
+        self.weight = self.weight - learning_rate * self.acc_weight/batch_size
+        self.bias = self.bias - learning_rate * self.acc_bias/batch_size
+        
+    def params(self):
+        """
+        Print of information of the layer
+        """
+        print(f"Layer : {self.layer_type}")
+        print(f"Nb input : {self.nb_input}")
+        print(f"Nb output : {self.nb_output}")
+        print(f"Weight matrix shape : {self.weight.shape}")
+        print(f"Bias matrix shape : {self.bias.shape}")
+        print(f"Weight: {self.weight}")
+        print(f"Bias : {self.bias}")
+          
+class ReLu(Linear):
+    """
+    ReLu class (Inheritance of linear) : create a ReLu layer of the neural network
+    Use 2 time the xavier distribution to initialize weight,
+    bias initialize with 0 
+    Parameters :
+        nb_input -- number of neuron in input
+        nb_output -- number of neuron in output
+    """
+    def __init__(self, nb_input, nb_output):
+        Linear.__init__(self, nb_input, nb_output)
+        self.layer_type = "ReLu"
+        self.variance = 2.0 * 2.0 / (nb_input + nb_output)
+        self.weight = np.sqrt(self.variance)*torch.randn(nb_input, nb_output)
+
+    def activation(self, x):
+        """
+        ReLu activation function output = maximum between 0 and input
+        Parameters : 
+            x -- input value
+        Retrun : 
+            out -- result of activation for x
+        """
+        out = (x>0).int()*x
+        return out
+    
+    def d_activation(self, x):
+        """
+        Derivative of ReLu activation function
+        Parameters : 
+            x -- value to compute the dervate activation
+        Retrun : 
+            out -- result of derivate activation for x
+        """
+        #print("ReLu derivate activation function")
+        out = (x>0).int()
+        return out
+    
+class TanH(Linear):
+    """
+    TanH class (Inheritance of linear) : create a TanH layer of the neural network
+    Use the xavier distribution to initialize weight,
+    bias initialize with 0 
+    Parameters :
+        nb_input -- number of neuron in input
+        nb_output -- number of neuron in output
+    """
+    def __init__(self, nb_input, nb_output):
+        Linear.__init__(self, nb_input, nb_output)
+        self.layer_type = "TanH"
+        self.variance = 2.0 / (nb_input + nb_output)
+        self.weight = np.sqrt(self.variance)*torch.randn(nb_input, nb_output)
+
+    def activation(self,x):
+        """
+        TanH activation function 
+        Parameters : 
+            x -- input value
+        Retrun : 
+            out -- result of activation for x
+        """
+        out = np.tanh(x)
+        return out
+    
+    def d_activation(self, x):
+        """
+        Derivate of TanH activation function 
+        Parameters : 
+            x -- value to compute the dervate activation
+        Retrun : 
+            out -- result of derivate activation for x
+        """
+        #print("TanH derivate activation function")
+        out = 1 - (np.tanh(x)).pow(2)
+        return x
---
+++ Proj2/neural_network.py
@@ -0,0 +1,130 @@
+# Import
+import numpy as np
+import torch
+
+class NeuralNetwork:
+    """
+    NeuralNetwork class : create a neural network, have to be configure with sequential
+    before using it
+    """
+    def __init__(self):
+        self.layers = []
+        self.graph_loss = []
+    
+    def sequential(self,*args):
+        """
+        Construct the neural network with layer
+        Parameters : 
+            *args -- constructor of each layer needed for the neural network
+        """
+        for arg in args:
+            self.layers.append(arg)
+
+    def run(self, input_data):
+        """
+        Compute the output of the neural network 
+        Parameters : 
+            input_data -- input value
+        Retrun : 
+            output -- result of neural network
+        """
+        output = input_data
+        for layer in self.layers:
+            output = layer.forward_pass
+        return output
+    
+    def loss(self, x, target):
+        """
+        Return the MSE loss between x and target
+        Parameters : 
+            x -- input value
+            target -- target value
+        Retrun : 
+            out -- MSE loss between x and target
+        """
+        out = torch.mean((target-x).pow(2),0)
+        return out
+    
+    def d_loss(self, x, target):
+        """
+        Return the derivative of MSE loss between x and target
+        Parameters : 
+            x -- input value
+            target -- target value
+        Retrun : 
+            out -- derivative of MSE loss between x and target
+        """
+        out = 2.0 * (x - target)/len(x)
+        return out
+        
+
+    def test_error(self, test_input, test_label):
+        """
+        Compute the number of error output of the neural network and the a label
+        Parameters : 
+            test_input -- input value before passing in the neural network
+            test_label -- Label to compare to the output of the neural network
+        Retrun : 
+            error -- number of error of classification
+        """
+        output = test_input
+        for layer in self.layers:
+            output = layer.forward_pass(output)
+        _, test2 = output.max(dim=1)
+        error = np.sum([0 if x != y else 1 for x,y in zip(test2, test_label)])
+        return error
+
+    def params(self):
+        """
+        Print information of the neural network
+        """
+        print("**** Neural network parameters ****")
+        for i, layer in enumerate(self.layers):
+            print(f"Layer : {i+1}")
+            layer.params()
+
+    def train_network(self, train_set, train_output, epochs, batch_size, learning_rate, print_error=False, test_set=None, test_label=None):
+        """
+        Train neural network
+        Parameters : 
+            train_set -- Set of input to train the neutal network matrix(nb, dim)
+            train_output -- Set of output to compute the loss matrix(nb, dim)
+            epochs -- Number of time to use train_set to train the neural network
+            batch_size -- Number of input to take in acompt before updating the parameters must be a integer division from epochs
+            learning_rate -- Learning rate of the neural network
+            print_error -- Boolean, default False, True print the loss and % of error at each epochs
+            test_set -- Default none, have to be set if print_error=True, set of input to test the neural network
+            test_label -- Defaulf none, have to be set if print_error=True, set of label to test the neural network
+        """
+        for k in range(epochs):
+            acc_loss = 0
+
+            for i in range((int)(train_set.size(0)/batch_size)):
+                
+                for layer in self.layers:
+                    layer.acc_weight = 0
+                    layer.acc_bias = 0
+
+                for n in range(batch_size):
+                    # Forward
+                    output = train_set[(i*batch_size)+n]
+                    for layer in self.layers:
+                        output = layer.forward_pass(output)
+                    
+                    # Loss Calculus
+                    acc_loss = acc_loss + self.loss(output, train_output[(i*batch_size)+n])
+                    
+                    # Backward
+                    dl_dy = self.d_loss(output, train_output[(i*batch_size)+n])
+                    for layer in reversed(self.layers):
+                        dl_dy = layer.backward_pass(dl_dy)
+
+                # Update parameters
+                for layer in self.layers:
+                    layer.update_parameters(learning_rate, batch_size)
+            # Log loss
+            self.graph_loss.append(acc_loss)
+
+            if print_error:
+                error= self.test_error(test_set, test_label)
+                print(f"Epoch : {k+1}, Loss : {acc_loss:6.2f}, Error : {error/test_set.size(0)*100.0:6.2f}%")
